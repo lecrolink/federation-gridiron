3e1ad5e3755a2accdb728035240145bccbc29566
Joe <jblecrone@gmail.com>
2026-06-20 15:39:33 +0000

    Configure MySQL database

--- federationgridiron/settings.py
+++ federationgridiron/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'federationgridiron.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "lecrolink$fg",
+        "USER": "lecrolink",
+        "PASSWORD": "wHiPPerferG888",
+        "HOST": "lecrolink.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
     }
 }
 
